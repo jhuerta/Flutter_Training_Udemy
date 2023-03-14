cedd33ffdc99b038c0a776d54c6f640228b40308
Juan Huerta <HuertaJ@swatchgroup.net>
2023-03-14 16:01:30 +0100

    Latest update - Lesson 30th

--- Projects/second_app_flutter_complete_guide/lib/main.dart
+++ Projects/second_app_flutter_complete_guide/lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => {MyApp()};
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello World!"));
+    // return MaterialApp(home: Text("Hello World! SDFAS"));
+    var questions = [
+      "One",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("This is the title"),
+            ),
+            body: Column(
+                children: [Text('The question'), RaisedButton("The Button")])
+            //Text("This is the body"),
+            ));
   }
 }
